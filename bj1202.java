c4074d8567f37be71c9afcae3b7013060141d966
sjaqjwor <lsklsk45@naver.com>
2018-01-08 13:43:56 +0900

    bj1202 complete

--- bj1202.java
+++ bj1202.java
@@ -1,60 +1,66 @@
 package baeckjoon;
 
-import java.lang.reflect.Array;
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Scanner;
+import java.util.Comparator;
+
+import java.util.PriorityQueue;
+
+import java.util.StringTokenizer;
 
 public class bj1202 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-			Scanner sc = new Scanner(System.in);
-			int n = sc.nextInt();
-			int m = sc.nextInt();
-			Jewerly j[] = new Jewerly[n];
-			int b[] = new int [m];
-			
-			for(int a=0;a<n;a++) {
-				int b1 =sc.nextInt();
-				int c = sc.nextInt();
-				j[a] = new Jewerly(b1,c);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		long j[][] = new long[n][2];
+		long b[] = new long[m];
+
+		for (int a = 0; a < n; a++) {
+			st = new StringTokenizer(br.readLine().trim());
+			long w = Integer.parseInt(st.nextToken());
+			long c = Integer.parseInt(st.nextToken());
+			j[a][0] = w;
+			j[a][1] = c;
+		}
+		for (int i = 0; i < m; i++) {
+			b[i] = Integer.parseInt(br.readLine().trim());
+		}
+
+		Comparator<long[]> comp = new Comparator<long[]>() {
+			@Override
+			public int compare(long[] o1, long[] o2) {
+				return (int) (o1[0] - o2[0]);
 			}
-			for(int a=0;a<m;a++) {
-				b[a]=sc.nextInt();
+		};
+		Arrays.sort(j, comp);
+		Arrays.sort(b);
+		PriorityQueue<Long> q = new PriorityQueue<>(1, Collections.reverseOrder());
+
+		int start = 0;
+		long min = 0;
+
+		for (int a = 0; a < m; a++) {
+			while (start < n && b[a] >= j[start][0]) {
+				q.offer(j[start][1]);
+				start++;
 			}
-			Arrays.sort(j);
-			
-			
-			
-			int start=0;
-			int min=0;
-			for(int a=m-1;a>=0;a--) {
-				if(j[start].weight<=b[a]) {
-					min+=j[start].cost;
-					start++;
-				}
+
+			if (!q.isEmpty()) {
+				min += q.poll();
 			}
-			System.out.println(min);
-			
-			
+		}
+
+		System.out.println(min);
+
 	}
 
-}
-class Jewerly implements Comparable<Jewerly>{
-	int weight;
-	int cost;
-	
-	public Jewerly(int w, int c) {
-		this.weight=w;
-		this.cost=c;
-	}
-
-	@Override
-	public int compareTo(Jewerly o) {
-		// TODO Auto-generated method stub
-		return this.cost-o.cost > 0 ? -1: 1;
-	}
-	
-	
 }
