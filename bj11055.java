8b5a1f45c9a893640e652ced28e252ab44d9b6d8
sjaqjwor <lsklsk45@naver.com>
2018-10-19 16:36:13 +0900

    bj11055

---
+++ bj11055.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class bj11055 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        int su[] = new int[n];
+
+
+        for(int a=0;a<n;a++){
+            arr[a]=sc.nextInt();
+            su[a]=arr[a];
+        }
+
+        for(int a=0;a<n;a++){
+            for(int b=0;b<a;b++){
+                if(arr[b]<arr[a]){
+                    su[a]=Math.max(su[a],arr[a]+su[b]);
+                }
+            }
+        }
+
+        int max=0;
+       for(int k : su){
+           if(k>max){
+               max=k;
+           }
+       }
+       System.out.print(max);
+    }
+}
