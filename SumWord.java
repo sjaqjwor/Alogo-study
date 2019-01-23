c9cb3389aa86631a7c2d783a36581fc15f00f41e
sjaqjwor <lsklsk45@naver.com>
2019-01-23 16:44:34 +0900

    sumword

--- SumWord.java
+++ SumWord.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class coupang3 {
+public class SumWord {
 
     public static void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
