84cc10dbd0d48ab464d7d52f2dde8d7e5bd3f771
sjaqjwor <lsklsk45@naver.com>
2019-02-04 21:41:24 +0900

    수젖ㅇ

--- KakaoTest1.java
+++ KakaoTest1.java
@@ -4,6 +4,7 @@ public class KakaoTest1 {
 
     public static void main(String[] args) {
         String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+
         LinkedList<String> list = new LinkedList<>();
         solution(record);
     }
